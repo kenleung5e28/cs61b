8ce331b62eb4ee275358c7ade024e189fa1ca23a
Ken Leung <kenleung5e28@gmail.com>
2022-05-02 17:52:08 +0800

    first commit

---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,38 @@
+public class LinkedListDeque<T> {
+  public void addFirst(T item) {
+    // TODO
+  }
+
+  public void addLast(T item) {
+    // TODO
+  }
+
+  public boolean isEmpty() {
+    // TODO
+    return false;
+  }
+
+  public int size() {
+    // TODO
+    return 0;
+  }
+
+  public void printDeque() {
+    // TODO
+  }
+
+  public T removeFirst() {
+    // TODO
+    return null;
+  }
+
+  public T removeLast() {
+    // TODO
+    return null;
+  }
+
+  public T get(int index) {
+    // TODO
+    return null;
+  }
+}
