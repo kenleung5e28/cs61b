add8332ab26dcf1657855c3f1ee4d65599c07644
Ken Leung <kenleung5e28@gmail.com>
2022-05-04 11:23:34 +0800

    add bare-bone for ArrayDeque class

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,19 @@
+public class ArrayDeque<T> {
+  public ArrayDeque() {}
+
+  public void addFirst(T item) {}
+
+  public void addLast(T item) {}
+
+  public boolean isEmpty() {}
+
+  public int size() {}
+
+  public void printQueue() {}
+
+  public T removeFirst() {}
+
+  public T removeLast() {}
+
+  public T get(int index) {}
+}
