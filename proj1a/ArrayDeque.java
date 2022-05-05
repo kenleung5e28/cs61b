bd1ede6fb4b03e62ffbf407549cde374c45c23fe
Ken Leung <kenleung5e28@gmail.com>
2022-05-05 11:42:49 +0800

    implement get, addFirst, removeFirst; fix printDeque

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,3 +1,9 @@
+/*
+* TODO:
+* 1. implement expanding items array upon usage factor exceeds a threshold
+* 2. implement shrinking items array upon usage factor falls below a threshold
+*/
+
 public class ArrayDeque<T> {
   private static final int INIT_SIZE = 8;
 
@@ -7,35 +13,57 @@ public class ArrayDeque<T> {
 
   public ArrayDeque() {
     items = (T[])new Object[INIT_SIZE];
-    nextFirst = 0;
-    nextLast = INIT_SIZE - 1;
+    nextFirst = INIT_SIZE - 1;
+    nextLast = 0;
   }
 
-  public void addFirst(T item) {}
+  public void addFirst(T item) {
+    // temp behavior: throw exception when items array is full
+    if (size() == items.length) {
+      throw new RuntimeException("unable to add new element when deque is full");
+    }
 
-  public void addLast(T item) {}
+    items[nextFirst] = item;
+    nextFirst = decIndex(nextFirst);
+  }
+
+  public void addLast(T item) {
+    // TODO
+  }
 
   public boolean isEmpty() { return size() == 0; }
 
   public int size() { return wrapIndex(nextLast - nextFirst) - 1; }
 
   public void printDeque() {
-    for (int i = indexIncrement(nextFirst); i < nextLast; i = indexIncrement(i)) {
-      System.out.print(items[i].toString() + " ");
+    int count = size();
+    for (int i = 0; i < count; i++) {
+      System.out.print(get(i).toString() + " ");
     }
     System.out.println();
   }
 
   public T removeFirst() {
-    return null;
+    if (isEmpty()) {
+      throw new RuntimeException("unable to remove element when deque is empty");
+    }
+
+    T result = get(0);
+    nextFirst = incIndex(nextFirst);
+    return result;
   }
 
   public T removeLast() {
+    // TODO
     return null;
   }
 
   public T get(int index) {
-    return null;
+    if (index < 0 || index >= size()) {
+      throw new ArrayIndexOutOfBoundsException("index out of bound");
+    }
+    int realIndex = wrapIndex(nextFirst + 1 + index);
+    return items[realIndex];
   }
 
 
@@ -48,11 +76,11 @@ public class ArrayDeque<T> {
     }
     return index;
   }
-  private int indexIncrement(int index) {
+  private int incIndex(int index) {
     return wrapIndex(index + 1);
   }
 
-  private int indexDecrement(int index) {
+  private int decIndex(int index) {
     return wrapIndex(index - 1);
   }
 }
