cb3509333e6ccc9f53bd835938b1d05f42e081bd
Ken Leung <kenleung5e28@gmail.com>
2022-05-11 23:35:48 +0800

    implement OffByOne

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -2,11 +2,37 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestOffByOne {
-    /*
     // You must use this CharacterComparator and not instantiate
     // new ones, or the autograder might be upset.
     static CharacterComparator offByOne = new OffByOne();
 
     // Your tests go here.
-    Uncomment this class once you've created your CharacterComparator interface and OffByOne class. **/
+    @Test
+    public void testOffByOneLessByOne() {
+        assertTrue(offByOne.equalChars('x', 'y'));
+    }
+
+    @Test
+    public void testOffByOneMoreByOne() {
+        assertTrue(offByOne.equalChars('b', 'a'));
+    }
+    @Test
+    public void testOffByOneSameChar() {
+        assertFalse(offByOne.equalChars('a', 'a'));
+    }
+
+    @Test
+    public void testOffByOneLessByTwo() {
+        assertFalse(offByOne.equalChars('i', 'k'));
+    }
+
+    @Test
+    public void testOffByOneMoreByThree() {
+        assertFalse(offByOne.equalChars('3', '0'));
+    }
+
+    @Test
+    public void testOffByOneCaseInsensitive() {
+        assertFalse(offByOne.equalChars('C', 'd'));
+    }
 }
