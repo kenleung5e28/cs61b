40427d8f35c3fb6390e53254818c8ac23efcd96b
Ken Leung <kenleung5e28@gmail.com>
2022-05-13 11:42:13 +0800

    add tests for ArrayRingBuffer

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -8,8 +8,54 @@ import static org.junit.Assert.*;
 
 public class TestArrayRingBuffer {
     @Test
-    public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+    public void testCapacity() {
+        ArrayRingBuffer<Integer> arb1 = new ArrayRingBuffer<>(5);
+        assertEquals(5, arb1.capacity());
+        ArrayRingBuffer<Integer> arb2 = new ArrayRingBuffer<>(20);
+        assertEquals(20, arb2.capacity());
+    }
+    @Test
+    public void testEnqueue() {
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(10);
+        arb.enqueue(100);
+        assertEquals(1, arb.fillCount());
+        arb.enqueue(200);
+        arb.enqueue(300);
+        assertEquals(3, arb.fillCount());
+    }
+
+    @Test
+    public void testDeque() {
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(10);
+        arb.enqueue(100);
+        arb.enqueue(200);
+        arb.enqueue(300);
+        int item1 = arb.dequeue();
+        assertEquals(2, arb.fillCount());
+        assertEquals(100, item1);
+        int item2 = arb.dequeue();
+        assertEquals(1, arb.fillCount());
+        assertEquals(200, item2);
+    }
+
+    @Test
+    public void testPeek() {
+        ArrayRingBuffer<String> arb = new ArrayRingBuffer<>(10);
+        arb.enqueue("First");
+        arb.enqueue("Second");
+        String item = arb.peek();
+        assertEquals(2, arb.fillCount());
+        assertEquals("First", item);
+    }
+
+    @Test
+    public void testFillCount() {
+        ArrayRingBuffer<String> arb = new ArrayRingBuffer<>(10);
+        arb.enqueue("First");
+        arb.enqueue("Second");
+        arb.dequeue();
+        arb.dequeue();
+        assertEquals(0, arb.fillCount());
     }
 
     /** Calls tests for ArrayRingBuffer. */
