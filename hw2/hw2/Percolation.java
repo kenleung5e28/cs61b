f66b9ef7b5009812b82fa0420a8b0d6431799c1e
Ken Leung <kenleung5e28@gmail.com>
2022-05-23 23:03:03 +0800

    init hw2

---
+++ hw2/hw2/Percolation.java
@@ -0,0 +1,43 @@
+package hw2;
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {
+        // TODO
+    }
+
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        // TODO
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        // TODO
+        return false;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        // TODO
+        return false;
+    }
+
+    // number of open sites
+    public int numberOfOpenSites() {
+        // TODO
+        return 0;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        // TODO
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
