778c9c5afbf6999b72ac569dcc2e46237e07a780
Ken Leung <kenleung5e28@gmail.com>
2022-05-26 23:20:27 +0800

    WIP

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,26 +1,40 @@
 package hw2;
 
-public class PercolationStats {
-    /*
-    // perform T independent experiments on an N-by-N grid
-    public PercolationStats(int N, int T, PercolationFactory pf) {
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
+public class PercolationStats {
+    // perform T independent experiments on an N-by-N grid
+    private final double mean;
+    private final double stddev;
+    private final double confidenceLow;
+    private final double confidenceHigh;
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("N and T must be positive.");
+        }
+        double[] fractions = new double[T];
+        for (int i = 0; i < T; i++) {
+            Percolation perc = pf.make(N);
+            while (!perc.percolates()) {
+
+            }
+        }
     }
     // sample mean of percolation threshold
     public double mean() {
-
+        return mean;
     }
     // sample standard deviation of percolation threshold
     public double stddev() {
-
+        return stddev;
     }
     // low endpoint of 95% confidence interval
     public double confidenceLow() {
-
+        return confidenceLow;
     }
     // high endpoint of 95% confidence interval
     public double confidenceHigh() {
-
+        return confidenceHigh;
     }
-     */
 }
