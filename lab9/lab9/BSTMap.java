2424ca0fea412433b36f6f6f3cd80cea95aab7bb
Ken Leung <kenleung5e28@gmail.com>
2022-05-28 17:37:00 +0800

    implement put method

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -70,7 +70,18 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
       * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
      */
     private Node putHelper(K key, V value, Node p) {
-        throw new UnsupportedOperationException();
+        if (p == null) {
+            return new Node(key, value);
+        }
+        int sign = key.compareTo(p.key);
+        if (sign < 0) {
+            p.left = putHelper(key, value, p.left);
+        } else if (sign > 0) {
+            p.right = putHelper(key, value, p.right);
+        } else {
+            p.value = value;
+        }
+        return p;
     }
 
     /** Inserts the key KEY
@@ -78,7 +89,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        root = putHelper(key, value, root);
     }
 
     /* Returns the number of key-value mappings in this map. */
