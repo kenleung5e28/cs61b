664612d67b4573fceef7f2e5c5adaed60de84179
Ken Leung <kenleung5e28@gmail.com>
2022-06-08 13:27:56 +0800

    fix changePriority

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -225,7 +225,7 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
         contents[size] = null;
         size -= 1;
         if (index < size) {
-            swim(index);
+            sink(index);
         }
         insert(item, priority);
     }
@@ -348,29 +348,22 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
         assertEquals("x7", pq.contents[7].myItem);
     }
 
-//    @Test
-//    public void testChangePriority() {
-//        ArrayHeap<String> pq = new ArrayHeap<>();
-//        pq.size = 7;
-//        for (int i = 1; i <= 7; i += 1) {
-//            pq.contents[i] = new ArrayHeap<String>.Node("x" + i, i);
-//        }
-//        System.out.println("PQ before priority change:");
-//        System.out.println(pq);
-//
-//        // Change item x6's priority to a low value.
-//
-//        pq.changePriority("x3", 10);
-//        System.out.println("PQ after priority change");
-//        System.out.println(pq);
-//        assertEquals("x6", pq.contents[1].myItem);
-//        assertEquals("x2", pq.contents[2].myItem);
-//        assertEquals("x1", pq.contents[3].myItem);
-//        assertEquals("x4", pq.contents[4].myItem);
-//        assertEquals("x5", pq.contents[5].myItem);
-//        assertEquals("x3", pq.contents[6].myItem);
-//        assertEquals("x7", pq.contents[7].myItem);
-//    }
+    @Test
+    public void testDecreasePriority() {
+        ArrayHeap<String> pq = new ArrayHeap<>();
+        pq.size = 7;
+        for (int i = 1; i <= 7; i += 1) {
+            pq.contents[i] = new ArrayHeap<String>.Node("x" + i, i);
+        }
+        System.out.println("PQ before priority change:");
+        System.out.println(pq);
+
+        // Change item x6's priority to a low value.
+
+        pq.changePriority("x1", 10);
+        System.out.println("PQ after priority change");
+        System.out.println(pq);
+    }
 
     @Test
     public void testSink() {
