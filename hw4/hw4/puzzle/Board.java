9edc438f26764c228b47482a4666b42a97534265
Ken Leung <kenleung5e28@gmail.com>
2022-06-11 00:41:46 +0800

    WIP

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,6 +1,31 @@
 package hw4.puzzle;
 
-public class Board {
+public class Board implements WorldState {
+    private int[][] tiles;
+    private int N;
+
+    @Override
+    public int estimatedDistanceToGoal() {
+        // TODO
+        return 0;
+    }
+
+    @Override
+    public Iterable<WorldState> neighbors() {
+        int x0;
+        int y0;
+        outermost:
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (tiles[i][j] == 0) {
+                    x0 = i;
+                    y0 = j;
+                    break outermost;
+                }
+            }
+        }
+        // TODO
+    }
 
     /** Returns the string representation of the board. 
       * Uncomment this method. */
