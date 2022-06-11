843933ec10bcd4ee692748549a2a287d45397b78
Ken Leung <kenleung5e28@gmail.com>
2022-06-11 11:26:11 +0800

    implement neighbors

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,8 +1,15 @@
 package hw4.puzzle;
 
+import java.util.ArrayList;
+
 public class Board implements WorldState {
     private int[][] tiles;
     private int N;
+
+    public Board(int[][] tiles) {
+        this.tiles = tiles;
+        // TODO
+    }
 
     @Override
     public int estimatedDistanceToGoal() {
@@ -12,8 +19,8 @@ public class Board implements WorldState {
 
     @Override
     public Iterable<WorldState> neighbors() {
-        int x0;
-        int y0;
+        int x0 = Integer.MIN_VALUE;
+        int y0 = Integer.MIN_VALUE;
         outermost:
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
@@ -24,7 +31,34 @@ public class Board implements WorldState {
                 }
             }
         }
-        // TODO
+        ArrayList<WorldState> neighbors = new ArrayList<>();
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                if (Math.abs(dx) + Math.abs(dy) != 1) {
+                    continue;
+                }
+                int x = x0 + dx;
+                int y = y0 + dy;
+                if (x < 0 || x >= N || y < 0 || y >= N) {
+                    continue;
+                }
+                int[][] swapped = new int[N][];
+                for (int i = 0; i < N; i++) {
+                    swapped[i] = new int[N];
+                    for (int j = 0; j < N; j++) {
+                        if (i == x && j == y) {
+                            swapped[i][j] = tiles[x0][y0];
+                        } else if (i == x0 && j == y0) {
+                            swapped[i][j] = tiles[x][y];
+                        } else {
+                            swapped[i][j] = tiles[i][j];
+                        }
+                    }
+                }
+                neighbors.add(new Board(swapped));
+            }
+        }
+        return neighbors;
     }
 
     /** Returns the string representation of the board. 
