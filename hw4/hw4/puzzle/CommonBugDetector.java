a7b213d36ab8d67ac6ec68cbc98ea90e9b4ce8af
Ken Leung <kenleung5e28@gmail.com>
2022-06-11 15:13:18 +0800

    fix style

--- hw4/hw4/puzzle/CommonBugDetector.java
+++ hw4/hw4/puzzle/CommonBugDetector.java
@@ -79,7 +79,7 @@ public class CommonBugDetector {
 
         AlphabetEasyPuzzle aep = new AlphabetEasyPuzzle('a');
         Solver s3 = new Solver(aep);
-        System.out.println(s3.numOfNodesEnqueued());
+        // System.out.println(s3.numOfNodesEnqueued());
         // if you print out the total number of items enqueued by s3
         // it should be approximately 25, not approximately 50.
     }
